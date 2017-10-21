46112d268e97f81f0d6f9954b7e533d0b226ca18
konojunya <junya03dance@gmail.com>
2017-10-21 11:08:18 +0900

    Hello from Vue

---
+++ client/src/timeSchedule/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="time-schedule">
+    <h1>Hello from Vue.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'time-schedule'
+}
+</script>
+
+<style lang="scss">
+#time-schedule {
+  color: red;
+}
+</style>
