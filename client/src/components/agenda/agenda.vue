64178f87aad202bc2bc900bc432fcfeacb01115e
ahaha0807 <b5331@oic.jp>
2017-10-22 01:37:28 +0900

    アジェンダのコンポーネントの作成

---
+++ client/src/components/agenda/agenda.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="agenda-list">
+        <agendaItem 
+            v-for="agendaItem in agenda" 
+            :item="agendaItem" 
+            :key="agendaItem.id" 
+            :progress="progress">
+        </agendaItem>
+        <nextAgenda :progress="progress" v-on:to-next-progress="changeProgress"></nextAgenda>
+    </ul>
+</template>
+
+<script>
+import AgendaItem from "./agendaItem.vue";
+import NextAgenda from "./nextAgenda.vue";
+
+export default {
+  props: ["agenda", "progress"],
+  components: {
+    AgendaItem,
+    NextAgenda
+  },
+  methods: {
+      changeProgress() {
+          this.$emit('change-progress', this.progress + 1)
+      }
+  }
+};
+</script>
---
+++ client/src/components/agenda/nextAgenda.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="button is-info" @click="next">NEXT > </button>
+</template>
+
+<script>
+export default {
+  props: ["progress"],
+  methods: {
+    next() {
+        this.$emit('to-next-progress')
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
