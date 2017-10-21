326d2200b4e4b2dc9a4b8fcee854041723b9450e
pei0804 <peeeei0804@gmail.com>
2017-10-21 15:10:37 +0900

    add: mongodb

--- server/go/src/app/server.go
+++ server/go/src/app/server.go
@@ -4,19 +4,25 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	"app/app"
+	"app/config"
 	"app/controller"
 	"app/design"
+
+	"path/filepath"
 
 	"github.com/deadcheat/goacors"
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/middleware"
+	mgo "gopkg.in/mgo.v2"
 )
 
 // Server 実行に必要な値を保持している
 type Server struct {
 	service *goa.Service
+	mongodb *mgo.Session
 }
 
 // NewServer Server構造体を作成する
@@ -26,9 +32,21 @@ func NewServer(s *goa.Service) *Server {
 	}
 }
 
+// loadConfig 設定ファイルの読み込み
+func (s *Server) loadConfig(settingFolder string, env string) {
+	cs, err := config.NewMongodbConfigsFromFile(filepath.Join(settingFolder, "mongodb.yml"))
+	if err != nil {
+		log.Fatalf("cannot open mongodb configuration. exit. %s", err)
+	}
+	s.mongodb, err = cs.MongodbOpen(env)
+	if err != nil {
+		log.Fatalf("mongodb initialization failed: %s", err)
+	}
+}
+
 func (s *Server) mountController() {
 	// Mount "rooms" controller
-	room := controller.NewRoomsController(s.service)
+	room := controller.NewRoomsController(s.service, s.mongodb)
 	app.MountRoomsController(s.service, room)
 	// Mount "front" controller
 	front := controller.NewFrontController(s.service)
@@ -52,11 +70,13 @@ func (s *Server) mountMiddleware(env string) {
 func main() {
 	service := goa.New(design.REPO)
 	var (
-		port = flag.String("port", ":8080", "addr to bind")
-		env  = flag.String("env", "production", "実行環境 (production, develop)")
+		port    = flag.String("port", ":8080", "addr to bind")
+		env     = flag.String("env", "develop", "実行環境 (production, develop)")
+		confDir = flag.String("confDir", "./config", "設定ファイルの場所")
 	)
 	flag.Parse()
 	s := NewServer(service)
+	s.loadConfig(*confDir, *env)
 	s.mountMiddleware(*env)
 	s.mountController()
 
