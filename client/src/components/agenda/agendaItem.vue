0ab205d0fa2b3d3f1f2e56aae5faecea72aaa283
ahaha0807 <b5331@oic.jp>
2017-10-22 01:26:32 +0900

    アジェンダviewのアイテムと詳細のコンポーネントの追加

---
+++ client/src/components/agenda/agendaItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <li class="agenda-item" :data-agenda-id="item.id">
+        <agendaInfo :title="item.title" :goal="item.goal" :time="item.time" :class="{progressing: isProgressing}"></agendaInfo>
+        <template v-if="item.child">
+            <ul class="agenda-child-list">
+                <agenda-item v-for="agenda in item.child" :item="agenda" :key="agenda.id" :progress="progress"></agenda-item>
+            </ul>
+        </template>
+  </li>
+</template>
+
+<script>
+import AgendaInfo from "./agendaInfo.vue";
+
+export default {
+  props: ["item", "progress"],
+  components: {
+    AgendaInfo
+  },
+//   data() {
+//       return {
+//           isProgressing: false
+//       }
+//   },
+//   mounted() {
+//       if (this.item.id === this.progress) {
+//           this.isProgressing = true
+//       }
+//   },
+  computed: {
+      isProgressing() {
+          return this.item.id === this.progress
+      }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.progressing {
+  background-color: orange;
+}
+</style>
+
